734e3b90a341b3d194dd5534bcb09a634866087c
jennifer6745 <131548139+jennifer6745@users.noreply.github.com>
2024-01-11 12:46:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//c'est une fonction fenêtre//
+fenêtre.addEventListener('charger', function () {
+    //definie l'id et la 2D//
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    //permet de regler la taille de l'image : canvas.width = 734; canvas.height = 464;//
+    
+});
+//on mets en place une fonction qui permet de placer le peronnage comme la taille, le deplacement la rapidité..ETC//
